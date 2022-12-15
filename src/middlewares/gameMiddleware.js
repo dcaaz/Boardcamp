1e9489c8a6c2388d6991c90a7d9bb7b20d136b29
dcaaz <danicreaazevedo@gmail.com>
2022-12-14 22:43:19 -0300

    feat: applying good practices in the name of the received object

--- src/middlewares/gameMiddleware.js
+++ src/middlewares/gameMiddleware.js
@@ -3,28 +3,28 @@ import { gameJoiSchema } from "../models/gamesJoiModels.js";
 
 export async function gameValidation(req, res, next) {
 
-    const info = req.body;
+    const game = req.body;
 
-    const { error } = gameJoiSchema.validate(info, { abortEarly: false });
+    const { error } = gameJoiSchema.validate(game, { abortEarly: false });
 
     if (error) {
         const errors = error.details.map(detail => detail.message);
         return res.status(422).send(errors);
     }
 
-    const nameExist = await connectionDB.query("SELECT * FROM games WHERE name=$1", [info.name]);
+    const nameExist = await connectionDB.query("SELECT * FROM games WHERE name=$1", [game.name]);
 
     if (nameExist.rowCount > 0) {
         return res.sendStatus(409);
     }
 
-    const idExist = await connectionDB.query("SELECT * FROM categories WHERE id=$1", [info.categoryId]);
+    const idExist = await connectionDB.query("SELECT * FROM categories WHERE id=$1", [game.categoryId]);
 
     if (idExist.rowCount === 0) {
         return res.sendStatus(400);
     }
 
-    req.info = info;
+    req.info = game;
 
     next();
 }
